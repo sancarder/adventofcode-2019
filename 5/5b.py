e85c8faf46aa9d9c1aa7a678c1b4f875d005b32e
Mattias Gustafsson <magu1704@student.miun.se>
2019-12-08 22:34:00 +0100

    Day 5 complete

---
+++ 5/5b.py
@@ -0,0 +1,145 @@
+def read_input():
+    input = open('input.txt', 'r')
+    intCode = input.readline().strip()
+    intCodes = intCode.split(',')
+    intCodes = list(map(int, intCodes))
+    #print(intCodes)
+    opPos = 0
+    op=intCodes[opPos]
+    
+    return op, intCodes, opPos
+
+def getParamMode(op, param):
+    #Example op=10102 param = 2
+    opString =str(op)
+    if op < 10:
+        return 0
+    else:
+        if param == 1:
+            return int(opString[len(opString)-3])
+        elif param == 2:
+            if op < 1000:
+                return 0 
+            else:
+                return int(opString[len(opString)-4])
+        elif param == 3:
+            if op < 10000:
+                return 0 
+            else:
+                return int(opString[0])
+        
+def getValue(opPos, intcodes, op, param):
+    if getParamMode(op, param) == 0:
+        return intCodes[intCodes[opPos]]
+    else:
+        return intCodes[opPos]
+
+
+def addCodes(opPos, intCodes, op):
+    term1 = getValue(opPos+1, intCodes, op, 1)
+    term2 = getValue(opPos+2, intCodes, op, 2)
+    if getParamMode(op, 3) == 0:
+        intCodes[intCodes[opPos+3]] = term1 + term2 
+    else:
+        intCodes[opPos+3] = term1 + term2 
+
+
+def multiplyCodes(opPos, intCodes, op):
+    term1 = getValue(opPos+1, intCodes, op, 1)
+    term2 = getValue(opPos+2, intCodes, op, 2)
+    if getParamMode(op, 3) == 0:
+        intCodes[intCodes[opPos+3]] = term1 * term2
+    else:
+        intCodes[opPos+3] = term1 * term2
+
+def storeInput(opPos, intCodes, inputValue):
+    intCodes[intCodes[opPos+1]] = inputValue 
+
+def outPutValue(opPos, intCodes, op):
+    if getParamMode(op, 1) == 1:
+        print(intCodes[opPos+1])
+    else:
+        print(intCodes[intCodes[opPos+1]])    
+
+def jumpIfTrue(opPos, intCodes, op):
+    value = getValue(opPos+1, intCodes, op, 1)
+    if value == 0:
+        return 3, 0 #numberOfParams is 3 and nothing else happens
+    else:
+        return 0, getValue(opPos+2, intCodes, op, 2)
+
+def jumpIfFalse(opPos, intCodes, op):
+    value = getValue(opPos+1, intCodes, op, 1)
+    if value == 0:
+        return 0, getValue(opPos+2, intCodes, op, 2)
+    else:
+        return 3, 0 #numberOfParams is 3 and nothing else happens
+
+def lessThan(opPos, intCodes, op):
+    term1 = getValue(opPos+1, intCodes, op, 1)
+    term2 = getValue(opPos+2, intCodes, op, 2)
+    #print (term1 , " " , term2)
+    if term1 < term2:
+        intCodes[intCodes[opPos+3]] = 1
+    else:
+        intCodes[intCodes[opPos+3]] = 0
+
+def equalTo(opPos, intCodes, op):
+    term1 = getValue(opPos+1, intCodes, op, 1)
+    term2 = getValue(opPos+2, intCodes, op, 2)
+    if term1 == term2:
+        intCodes[intCodes[opPos+3]] = 1
+    else:
+        intCodes[intCodes[opPos+3]] = 0
+
+def parseOp(op):
+    if op < 10:
+        return op
+    else:
+        opString = str(op)
+        return int(opString[len(opString)-2:])
+
+def run_program(op, intCodes, opPos, inputValue):
+
+    while op != 99 and opPos < len(intCodes):
+       
+        opCode = parseOp(op)
+        #print("opPos: ", opPos, " opCode: ", opCode)
+        #print(intCodes)
+        if(opCode == 1):
+            addCodes(opPos, intCodes, op)
+            numberOfParams = 4
+        elif (opCode == 2):
+            multiplyCodes(opPos, intCodes, op)
+            numberOfParams = 4
+        elif (opCode == 3):
+            storeInput(opPos, intCodes, inputValue)
+            numberOfParams = 2
+        elif (opCode == 4):
+            outPutValue(opPos, intCodes, op)
+            numberOfParams = 2
+        elif (opCode == 5):
+            numberOfParams, opMove = jumpIfTrue(opPos, intCodes, op)
+            if opMove != 0:
+                opPos = opMove
+        elif (opCode == 6):
+            numberOfParams, opMove = jumpIfFalse(opPos, intCodes, op)
+            if opMove != 0:
+                opPos = opMove
+        elif (opCode == 7):
+            lessThan(opPos, intCodes, op)
+            numberOfParams = 4
+        elif (opCode == 8):
+            equalTo(opPos, intCodes, op)
+            numberOfParams = 4
+
+        opPos += numberOfParams 
+        op = intCodes[opPos]
+
+    return "Stopped"
+
+inputValue = 5
+#print(parseOp(102))
+op, intCodes, opPos = read_input()        
+answer = run_program(op, intCodes, opPos, inputValue)
+print(answer)
